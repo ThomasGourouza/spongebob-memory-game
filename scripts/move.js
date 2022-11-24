4cb88b789f55ea7795fe19e293780ee8b3d5d20f
Thomas <thomas.gourouza@gmail.com>
2022-11-25 00:19:03 +0100

    animation

--- scripts/move.js
+++ scripts/move.js
@@ -1,42 +1,21 @@
-// Make the DIV element draggable:
-dragElement(document.getElementById("bob"));
-dragElement(document.getElementById("patrick"));
-dragElement(document.getElementById("carlos"));
+const bob = document.getElementById("bob");
+const patrick = document.getElementById("patrick");
+const carlos = document.getElementById("carlos");
 
-function dragElement(elmnt) {
-  let pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+function moveToRight(restPixels, totalPixels) {
+  if (restPixels === undefined) {
+    restPixels = totalPixels;
+    totalPixels += bob.getBoundingClientRect().left;
   }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  if (restPixels < 1) {
+    return;
   }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+  const screenWidth = window.screen.width;
+  setTimeout(() => {
+    bob.style.left = `${((totalPixels - restPixels) * 100) / screenWidth}%`;
+    restPixels--;
+    moveToRight(restPixels, totalPixels);
+  }, 50);
 }
+
+// moveToRight(undefined, 300);
