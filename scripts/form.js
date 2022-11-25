168240cc3b5e630d8ebaa5505ee894946c0371c0
Thomas Gourouza <thomas.gourouza@de.markant.com>
2022-11-25 17:10:14 +0100

    jellyfish

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,7 @@
 const bob = getElement('#bob');
 const patrick = getElement('#patrick');
 const carlos = getElement('#carlos');
+const jellyfish = getElement('#jellyfish');
 const audio = getElement('audio');
 
 audio.volume = 0;
@@ -12,9 +13,28 @@ toggleAudio();
 function toggleAudio() {
     audio.volume = (audio.volume === 1) ? 0 : 1;
     [bob, patrick, carlos].forEach((character) => {
-        character.classList.add(audio.volume === 1 ? 'dance' : 'sad');
-        character.classList.remove(audio.volume === 1 ? 'sad' : 'dance');
+        (audio.volume === 1)
+        ? character.classList.replace('sad', 'dance')
+        : character.classList.replace('dance', 'sad');
     });
+}
+function playPatrick() {
+    if (jellyfish.className === 'goAway') {
+        return;
+    }
+    const currentState = (audio.volume === 1) ? 'dance' : 'sad';
+    if (patrick.classList.contains('player')) {
+        patrick.classList.replace('player', currentState);
+        jellyfish.className = 'goAway';
+        setTimeout(() => {
+            jellyfish.className = 'notHere';
+        }, 2000);
+    } else {
+        jellyfish.className = 'isHere';
+        setTimeout(() => {
+            patrick.classList.replace(currentState, 'player');
+        }, 1000);
+    }
 }
 
 /**
